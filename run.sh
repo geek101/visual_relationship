078350980790ea12508ffbb63c478dba5ff2e292
geek101 <powellm79@yahoo.com>
2018-11-29 12:31:41 -0800

    The patch for retinanet to save the inference output as numpy pickle file per label. Helper scripts to run retinanet training and inference.

---
+++ run.sh
@@ -0,0 +1,1 @@
+keras_retinanet/bin/train.py --imagenet-weights --freeze-backbone --no-evaluation --epochs 20  --image-min-side 343 --image-max-side 514 --batch-size 1 csv /home/powell/work/scpd/vision_project/visual_relationship/fullsize_train_annotations.csv /home/powell/work/scpd/vision_project/visual_relationship/fullsize_classes.csv
